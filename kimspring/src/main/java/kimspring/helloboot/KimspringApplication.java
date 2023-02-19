41193d7f31986013a201ff81e50948b708c29742
kks <kks@host.docker.internal>
2023-02-19 22:49:54 +0900

    서블릿 컨테이너 구동(톰캣)

--- kimspring/src/main/java/kimspring/helloboot/KimspringApplication.java
+++ kimspring/src/main/java/kimspring/helloboot/KimspringApplication.java
@@ -1,7 +1,16 @@
 package kimspring.helloboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class KimspringApplication {
 
 	public static void main(String[] args) {
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		
+		webServer.start();
+		
 	}
 }
