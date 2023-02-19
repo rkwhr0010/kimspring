028a50aebc3aadf46b15dd22427f4e3220ef7494
kks <kks@host.docker.internal>
2023-02-19 13:53:12 +0900

    간단한 RestController 테스트

---
+++ kimspring/src/main/java/kimspring/helloboot/KimspringApplication.java
@@ -0,0 +1,13 @@
+package kimspring.helloboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimspringApplication.class, args);
+	}
+
+}
