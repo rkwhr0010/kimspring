de32861c3d4c7db8986467551f189f217fd52716
kks <kks@host.docker.internal>
2023-02-22 21:00:47 +0900

    자바 코드 구성 정보 사용, 어노테이션 지원 스프링 컨테이너 사용

--- kimspring/src/main/java/kimspring/helloboot/KimspringApplication.java
+++ kimspring/src/main/java/kimspring/helloboot/KimspringApplication.java
@@ -2,30 +2,41 @@ package kimspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class KimspringApplication {
-
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 	public static void main(String[] args) {
 		//스프링 컨테이너
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigServletWebApplicationContext applicationContext = 
+				new AnnotationConfigServletWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
 				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 				WebServer webServer = serverFactory.getWebServer(servletContext ->{
 					servletContext.addServlet("dispatcherServlet", 
-//							new DispatcherServlet(applicationContext)
 							new DispatcherServlet(this)
 							).addMapping("/*"); 
 				});
 				webServer.start();
 			}
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(KimspringApplication.class);
+//		applicationContext.regisTERBEAN(HELLOCONTROLLER.CLASS);
+//		APPLICATIONCONTEXT.REGISTERBEan(SimpleHelloService.class);
 		applicationContext.refresh();
 		
 	}
