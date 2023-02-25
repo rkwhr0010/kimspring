cda6714c5d511bdda4165b640d9f26610a8b8784
kks <kks@host.docker.internal>
2023-02-25 14:59:38 +0900

    SpringApplication 으로 전환 전 우리가 만든 MySpringApplication사용

--- kimspring/src/main/java/kimspring/helloboot/KimspringApplication.java
+++ kimspring/src/main/java/kimspring/helloboot/KimspringApplication.java
@@ -12,40 +12,18 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class KimspringApplication {
-	
 	@Bean
 	public ServletWebServerFactory servletWebServerFactory() {
 		return new TomcatServletWebServerFactory();
 	}
-	
 	@Bean 
 	public DispatcherServlet dispatcherServlet() {
 		return new DispatcherServlet();
 	}
 	
+	//스프링 부트 초기 시작 어플리케이션과 유사해졌다.
 	public static void main(String[] args) {
-		//스프링 컨테이너
-		AnnotationConfigServletWebApplicationContext applicationContext = 
-				new AnnotationConfigServletWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-				
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-				
-//				dispatcherServlet.setApplicationContext(this);
-				//스프링 컨테이너가 자동으로 등록해준다. , ApplicationContextAware 이용
-				
-				WebServer webServer = serverFactory.getWebServer(servletContext ->{
-					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-						.addMapping("/*"); 
-				});
-				webServer.start();
-			}
-		};
-		applicationContext.register(KimspringApplication.class);
-		applicationContext.refresh();
-		
+		MySpringApplication.run(KimspringApplication.class, args);
 	}
+	
 }
