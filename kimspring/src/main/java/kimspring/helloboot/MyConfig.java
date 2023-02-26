0540f0b8fa101daf13dc3ef0a42461aa918a9641
kks <kks@host.docker.internal>
2023-02-26 12:01:06 +0900

    합성 애노테이션 적용

--- kimspring/src/main/java/kimspring/helloboot/MyConfig.java
+++ kimspring/src/main/java/kimspring/helloboot/MyConfig.java
@@ -1,18 +1,21 @@
 package kimspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@MySpringBootAnnotation
-public class KimspringApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(KimspringApplication.class, args);
-	}
+@Configuration
+public class MyConfig {
+    @Bean
+    ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
 	
 }
