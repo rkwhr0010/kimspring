687a90e6d3f903a11dd0b9f178e080f14c0bbd7b
kks <kks@host.docker.internal>
2023-02-26 17:59:47 +0900

    자동 구성 정보 파일 분리, DeferredImportSelector

--- kimspring/src/main/java/kimspring/config/MyAutoConfigImportSelector.java
+++ kimspring/src/main/java/kimspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,23 @@
 package kimspring.config;
 
+import java.util.stream.StreamSupport;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector{
+	private final ClassLoader classLoader;
+	
+	public MyAutoConfigImportSelector(ClassLoader classLoader) {
+		super();
+		this.classLoader = classLoader;
+	}
+	
 	@Override
 	public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-		return new String[] {
-				"kimspring.config.autoconfig.DispatcherServletConfig",
-				"kimspring.config.autoconfig.TomcatServletWebServerConfig"
-		};
+		return StreamSupport
+				.stream(ImportCandidates.load(MyAutoConfiguration.class, classLoader).spliterator(), false)
+				.toArray(String[]::new);
 	}
 }
