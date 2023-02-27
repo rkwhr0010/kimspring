26afefeaa162eaa8a40807965fdd62fe5a6bc410
kks <kks@host.docker.internal>
2023-02-27 12:17:27 +0900

    스타터와 Jetty 서버 구성 추가(구동안됨 서블릿웹서버팩토리 충돌)

--- kimspring/src/main/java/kimspring/config/autoconfig/JettyServletWebServerConfig.java
+++ kimspring/src/main/java/kimspring/config/autoconfig/JettyServletWebServerConfig.java
@@ -1,16 +1,17 @@
 package kimspring.config.autoconfig;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 import kimspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-public class TomcatServletWebServerConfig {
-	@Bean("tomcatServletWebServerFactory")
+public class JettyServletWebServerConfig {
+	//명시적으로 이름을 안주면 메서드이름으로 등록된다.
+    @Bean("jettyServletWebServerFactory")
     ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
 }
