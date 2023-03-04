55596026f6bb0897ce5106d8e1f0160ec4626438
kks <rkwhr0010@naver.com>
2023-03-04 18:03:25 +0900

    프로퍼티 빈의 후처리기 도입

--- kimspring/src/main/java/kimspring/config/autoconfig/UndertowServletWebServerConfig.java
+++ kimspring/src/main/java/kimspring/config/autoconfig/UndertowServletWebServerConfig.java
@@ -1,30 +1,27 @@
 package kimspring.config.autoconfig;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 import kimspring.config.ConditionalMyOnClass;
+import kimspring.config.EnableMyConfigurationProperties;
 import kimspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("io.undertow.Undertow")
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class UndertowServletWebServerConfig {
-	
-	@Value("${server.port}")
-	int port;
-	@Value("${server.servlet.context-path}")
-	String contextPath;
-	
 	
     @Bean("undertowServletWebServerFactory")
     @ConditionalOnMissingBean
-    ServletWebServerFactory servletWebServerFactory() {
+    ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
     	UndertowServletWebServerFactory serverFactory = new UndertowServletWebServerFactory();
-    	serverFactory.setPort(port);
-    	serverFactory.setContextPath(contextPath);
+    	serverFactory.setPort(properties.getPort());
+    	serverFactory.setContextPath(properties.getContextPath());
         return serverFactory;
     }
+    
+    
 }
